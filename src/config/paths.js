4a574782ec520f8c1536b779ae430273b859780b
rak-mi <someonex666@gmail.com>
2025-11-04 10:37:00 -0500

    move settings to a config folder

--- src/config/paths.js
+++ src/config/paths.js
@@ -6,17 +6,18 @@
 export const paths = {
     // Base directories
     logs: './logs',
+    config: './config',
     data: './',
 
-    // JSON configuration files - saved in same directory as executable
-    settings: './settings.json',
-    users: './users.json',
-    windowConfig: './windowConfig.json',
-    bossHpWindowConfig: './boss-hp-window.json',
-    historyWindowConfig: './history-window.json',
-    settingsWindowConfig: './settings-window.json',
-    keybinds: './keybinds.json',
-    networkSettings: './networkSettings.json',
+    // JSON configuration files - saved in config directory
+    settings: './config/settings.json',
+    users: './config/users.json',
+    windowConfig: './config/windowConfig.json',
+    bossHpWindowConfig: './config/boss-hp-window.json',
+    historyWindowConfig: './config/history-window.json',
+    settingsWindowConfig: './config/settings-window.json',
+    keybinds: './config/keybinds.json',
+    networkSettings: './config/networkSettings.json',
 };
 
 /**
@@ -24,7 +25,7 @@ export const paths = {
  */
 export async function ensureDirectories() {
     const { promises: fsPromises } = await import('fs');
-    const dirs = [paths.logs];
+    const dirs = [paths.logs, paths.config];
 
     for (const dir of dirs) {
         try {
